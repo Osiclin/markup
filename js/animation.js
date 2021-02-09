725cf6b3460ec43200e2665edd9fd0fed7ea32df
osita <67206380+Osiclin@users.noreply.github.com>
2021-02-09 21:05:17 +0100

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -16,7 +16,7 @@ function showText() {
     $('#text1').slideDown(500).delay(10000).fadeOut();
     $('#text2').delay(10900).slideDown(500).delay(10000).fadeOut();
     $('#text3').delay(21900).slideDown(500).delay(10000).fadeOut();
-    setTimeout(showText, 33200);
+    setTimeout(showText, 33300);
 }
 
 showText();
